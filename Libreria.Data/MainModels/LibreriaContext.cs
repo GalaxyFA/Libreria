6cf83242b50303b0f92a906d234e09e4c7d8835f
GalaxyFA <flaviocesararauz@gmail.com>
2022-11-20 22:35:59 -0600

    A

--- Libreria.Data/MainModels/LibreriaContext.cs
+++ Libreria.Data/MainModels/LibreriaContext.cs
@@ -36,7 +36,7 @@ namespace Libreria.Data.MainModels
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Libreria;User Id=sa;Password=passw0rd*");
-            }
+            }//
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- Libreria/Gestion_Empleado.xaml.cs
+++ Libreria/Gestion_Empleado.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Libreria.Data;
+using Libreria.Data.Empleados;
 using Libreria.Data.MainModels;
 using System;
 using System.Collections.Generic;
@@ -24,13 +25,14 @@ namespace Libreria
     {
         private readonly LibreriaContext _libreriaContext;
         private readonly IRepository<Empleado> _repository;
-
+        Empleado registroEmpleado = new Empleado();
+        int id;
         public Gestion_Empleado(LibreriaContext libreriaContext, IRepository<Empleado> repository)
         {
             InitializeComponent();
             _libreriaContext = libreriaContext;
             _repository = repository;
-
+            MostrarLista();
             //
 
             //var empleados = _repository.GetAll();
@@ -46,32 +48,103 @@ namespace Libreria
 
         private void btnEliminar_Click(object sender, RoutedEventArgs e)
         {
-
+            if(dgEmpleados.SelectedIndex != -1)
+            {
+                registroEmpleado = (Empleado)dgEmpleados.SelectedItem;
+                _repository.Del(registroEmpleado.IdEmpleado);
+                MostrarLista();
+            }
         }
 
         private void btnCargar_Click(object sender, RoutedEventArgs e)
         {
-
+            if(dgEmpleados.SelectedIndex != -1)
+            {
+                registroEmpleado = (Empleado)dgEmpleados.SelectedItem;
+                txtId_empleado.Text = $"{registroEmpleado.IdEmpleado}";
+                txtPrimer_nombre.Text = registroEmpleado.PrimerNombre;
+                txtSegundo_nombre.Text = registroEmpleado.SegundoNombre;
+                txtPrimer_apellido.Text = registroEmpleado.PrimerApellido;
+                txtSegundo_apellido.Text = registroEmpleado.SegundoApellido;
+                txtTelefono.Text = registroEmpleado.Telefono;
+                txtDireccion.Text = registroEmpleado.Direccion;
+                txtTitulo.Text=registroEmpleado.Titulo;
+                btnEditar.IsEnabled = true;
+            }
+            else
+            {
+                MessageBox.Show("No hay Datos", "Aviso");
+            }
         }
+
+        
+            
+      
 
         private void btnEditar_Click(object sender, RoutedEventArgs e)
         {
+            registroEmpleado.IdEmpleado = Convert.ToInt32(txtId_empleado.Text);
+            registroEmpleado.PrimerNombre= txtPrimer_nombre.Text;
+            registroEmpleado.SegundoNombre = txtSegundo_nombre.Text;
+            registroEmpleado.PrimerApellido = txtPrimer_apellido.Text;
+            registroEmpleado.SegundoApellido = txtSegundo_apellido.Text;
+            registroEmpleado.Titulo = txtTitulo.Text;
+            registroEmpleado.Telefono = txtTelefono.Text;
+            registroEmpleado.Direccion = txtDireccion.Text;
+            _repository.Edit(registroEmpleado);
+            Limpiar();
+            btnEditar.IsEnabled = false;
+            MostrarLista();
 
         }
 
         private void btnNuevo_Click(object sender, RoutedEventArgs e)
         {
-
+            registroEmpleado.IdEmpleado = id;
+            registroEmpleado.PrimerApellido = txtPrimer_apellido.Text;
+            registroEmpleado.PrimerNombre = txtPrimer_nombre.Text;
+            registroEmpleado.SegundoNombre = txtSegundo_nombre.Text;
+            registroEmpleado.SegundoApellido = txtSegundo_apellido.Text;
+            registroEmpleado.Telefono = txtTelefono.Text;
+            registroEmpleado.Titulo = txtTitulo.Text;
+            registroEmpleado.Direccion = txtDireccion.Text;
+            _repository.Add(registroEmpleado);
+            _repository.Savechange();
+            Limpiar();
+            
+            MostrarLista();
         }
 
         private void btnRefrescar_Click(object sender, RoutedEventArgs e)
         {
-
+            MostrarLista();
         }
 
         private void btnBuscar_Click(object sender, RoutedEventArgs e)
         {
+            var res = from empleado in _libreriaContext.Empleados where 
+                      empleado.PrimerNombre.Contains(txtBuscar_nombre.Text) orderby
+                      empleado.PrimerNombre descending select empleado;
+            dgEmpleados.ItemsSource= _repository.GetByQuery(res);
 
+        }
+        private void MostrarLista()
+        {
+            var empleados = _repository.GetAll();
+            id = empleados.Count() + 1;
+            dgEmpleados.ItemsSource= _repository.GetAll();
+        }
+        private void Limpiar()
+        {
+            txtBuscar_nombre.Clear();
+            txtDireccion.Clear();
+            txtId_empleado.Clear();
+            txtPrimer_apellido.Clear();
+            txtPrimer_nombre.Clear();
+            txtSegundo_apellido.Clear();
+            txtSegundo_nombre.Clear();
+            txtTelefono.Clear();
+            txtTitulo.Clear();
         }
     }
 }
